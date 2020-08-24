283f75f30212ed4eed856674869906ea8e6d8396
SirTonyBaac <siripong.yiangyong@gmail.com>
2020-08-24 19:59:38 +0700

    [Edited]:

--- src/page/Review.js
+++ src/page/Review.js
@@ -71,7 +71,7 @@ class Review extends React.Component {
             ...this.state,
             txtEmail: res.data.data.email,
             txtFirstName: res.data.data.firstname,
-            imageUrl: <Avatar style={{ backgroundColor: '#282c34', color: 'white' }}>{res.data.data.email.substring(0, 1)}</Avatar>,
+            imageUrl: <Avatar style={{ backgroundColor: '#282c34', color: 'white' }}>{res.data.data.firstname.substring(0, 1)}</Avatar>,
           })
         } else {
           this.setState({
@@ -82,24 +82,56 @@ class Review extends React.Component {
           })
         }
       }
-      this.setState({
-        submitting: false,
-        value: '',
-        comments: [
-          {
-            avatar: <Avatar style={{ backgroundColor: '#282c34', color: 'white' }}>{this.state.txtEmail.substring(0, 1)}</Avatar>,
-            author: <span style={{ color: blue[6] }}>{this.state.txtFirstName}</span>,
-            datetime: moment().fromNow(),
-            content: "Hello",
-          },
-          {
-            avatar: <Avatar style={{ backgroundColor: '#282c34', color: 'white' }}>{this.state.txtEmail.substring(0, 1)}</Avatar>,
-            author: <span style={{ color: blue[6] }}>{this.state.txtFirstName}</span>,
-            datetime: moment().fromNow(),
-            content: "Sawatdee",
-          },
-          ...this.state.comments,
-        ],
+
+      var axios = require('axios');
+      var data = JSON.stringify(
+        {
+          "guideid": localStorage.getItem('idGuideInLocalStorage'),
+        }
+      );
+      console.log(data)
+  
+      var config = {
+        method: 'post',
+        url: 'https://fighto-api.topwork.asia/api/comment/get',
+        headers: { 
+          'Authorization': 'Bearer '+localStorage.getItem('idTokenInLocalStorage'), 
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Origin, Accept, Authorization, Content-Length, X-Requested-With'
+        },
+        data : data,
+        reponseType: { 
+          'Content-Type': 'application/json'
+        }
+      };
+      console.log(config.url)
+      console.log(config.headers)
+      
+      axios(config)
+      .then(res => {
+        console.log(JSON.stringify(res.data));
+        if (res.data.success == true) {
+          for (let i=0; i < res.data.data.commentList.length; i++) {
+            this.setState({
+              submitting: false,
+              value: '',
+              comments: [
+                {
+                  avatar: <Avatar style={{ backgroundColor: '#282c34', color: 'white' }}>{res.data.data.commentList[i].author.substring(0, 1)}</Avatar>,
+                  author: <span style={{ color: blue[6] }}>{res.data.data.commentList[i].author}</span>,
+                  datetime: res.data.data.commentList[i].datecomment,
+                  content: res.data.data.commentList[i].message,
+                },
+                ...this.state.comments,
+              ],
+            });
+          }
+        }
+      })
+      .catch(error =>{
+        console.log(error);
       });
     })
     .catch(error =>{
@@ -123,7 +155,7 @@ class Review extends React.Component {
           value: '',
           comments: [
             {
-              avatar: <Avatar style={{ backgroundColor: '#282c34', color: 'white' }}>{this.state.txtEmail.substring(0, 1)}</Avatar>,
+              avatar: <Avatar style={{ backgroundColor: '#282c34', color: 'white' }}>{this.state.txtFirstName.substring(0, 1)}</Avatar>,
               author: <span style={{ color: blue[6] }}>{this.state.txtFirstName}</span>,
               datetime: moment().fromNow(),
               content: <p>{this.state.value}</p>,
@@ -154,7 +186,7 @@ class Review extends React.Component {
         "guideid": localStorage.getItem('idGuideInLocalStorage'),
         "useridcomment": localStorage.getItem('idUserInLocalStorage'),
         "useremail": this.state.txtEmail,
-        "avatar": this.state.imageUrl,
+        "avatar": "",
         "author": this.state.txtFirstName,
         "datecomment": moment().fromNow(),
         "message": this.state.value,
